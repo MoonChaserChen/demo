d9d011b5406e8306a8fe75534c9711d62e362493
MoonChaserChen <moon_chaser@163.com>
2019-06-29 08:30:22 +0800

    demo cas client

---
+++ demo-cas-spring-boot-starter/demo-cas-client/src/main/java/ink/akira/demo/cas/CasClientApplication.java
@@ -0,0 +1,11 @@
+package ink.akira.demo.cas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CasClientApplication.class, args);
+    }
+}

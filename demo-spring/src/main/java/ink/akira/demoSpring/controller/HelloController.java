19b93385a56cb473a65ce3534d176fa1db602691
MoonChaserChen <moon_chaser@163.com>
2019-08-22 17:03:07 +0800

    demo-spring版本由5.1.4升级到5.1.9

--- demo-spring/src/main/java/ink/akira/demoSpring/controller/HelloController.java
+++ demo-spring/src/main/java/ink/akira/demoSpring/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @RequestMapping(value="/hello")
-    public JsonResult hello(String param1, String param2) {
+    public JsonResult hello(String param1, String[] param2) {
         logger.info("===== param1 = " + param1);
         logger.info("===== param2 = " + param2);
         return JsonResult.success("你好！");
